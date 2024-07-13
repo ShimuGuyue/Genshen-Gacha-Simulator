9084f3244d9a99fabb1f0f1278604c1bc3fa3baf
ShimuGuyue <2147539031@qq.com>
2024-07-13 13:50:38 +0800

    Add files via upload

--- 抽卡模拟器/源.cpp
+++ 抽卡模拟器/源.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <ctime>
+#include <vector>
+#include <iomanip>
 
 int A(0);
 bool AA(false);
@@ -11,6 +13,8 @@ int countA(0);
 int countAA(0);
 int countS(0);
 int countSS(0);
+
+std::vector<std::vector<std::string>> gachaRecords(1);
 
 void showMenu(void);
 void cardDraw(void);
@@ -33,9 +37,21 @@ void showMenu(void)
 	{
 		std::cout << "*****请选择你要抽卡的次数：" << std::endl;
 		std::cout << "     A)单抽     B)十连     C)查看当前保底情况     D)查看当前抽卡情况统计     Q)结束抽卡" << std::endl;
-		char mod;
-		std::cin >> mod;
-		std::cout << "------------------------------" << std::endl;;
+		std::string input;
+		std::cin >> input;
+		char mod('#');
+		for (char c : input)
+			if (c == 'a' || c == 'A' || c == 'b' || c == 'B' || c == 'c' || c == 'C' || c == 'd' || c == 'D' || c == 'q' || c == 'Q')
+			{
+				mod = c;
+				break;
+			}
+		if (input.size() > 1)
+		{
+			std::cout << "指令数过多，仅执行第一条合法指令！！！" << std::endl;
+			std::cout << "第一条合法指令：" << static_cast<char>(toupper(mod)) << std::endl;
+		}
+		std::cout << "------------------------------" << std::endl;		
 		if (mod == 'A' || mod == 'a')
 			cardDraw();
 		else if (mod == 'B' || mod == 'b')
@@ -58,12 +74,26 @@ void showMenu(void)
 		else if (mod == 'D' || mod == 'd')
 		{
 			std::cout << "当前抽卡数量统计：" << std::endl;
-			std::cout << "总抽数：" << countA + countAA + countS + countSS + countB << "抽" << std::endl;
-			std::cout << "B总数：" << countB << "个" << std::endl;
-			std::cout << "A总数：" << countA << "个" << std::endl;
-			std::cout << "AA总数：" << countAA << "个" << std::endl;
-			std::cout << "S总数：" << countS << "个" << std::endl;
-			std::cout << "SS总数：" << countSS << "个" << std::endl;
+			std::cout << "总抽数： " << countA + countAA + countS + countSS + countB << "\t抽" << std::endl;
+			std::cout << "B总数 ： " << countB << "\t个" << std::endl;
+			std::cout << "A总数 ： " << countA << "\t个" << std::endl;
+			std::cout << "AA总数： " << countAA << "\t个" << std::endl;
+			std::cout << "S总数 ： " << countS << "\t个" << std::endl;
+			std::cout << "SS总数： " << countSS << "\t个" << std::endl;
+			std::cout << "抽卡情况：" << std::endl;
+			for (std::vector<std::string> vs : gachaRecords)
+			{
+				int count(0);
+				for (std::string s : vs)
+				{
+					std::cout << std::setw(4) << std::left << s;
+					++count;
+					if (count % 10 == 0 && count && s != "SS")
+						std::cout << std::endl;
+				}
+				std::cout << std::endl;
+				std::cout << "--------------------------------------" << std::endl;
+			}
 		}
 		else if (mod == 'Q' || mod == 'q')
 		{
@@ -71,11 +101,8 @@ void showMenu(void)
 			break;
 		}
 		else
-			std::cout << "*****错误的选项！！！请重新输入：";
+			std::cout << "*****非法指令！！！请重新输入：";
 
-		//std::cout << "----------按下ENTER键继续:----------" << std::endl;
-		//getchar();
-		//getchar();
 		std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n";
 	}
 	getchar();
@@ -85,6 +112,7 @@ void showMenu(void)
 
 void cardDraw(void)
 {
+	int i(gachaRecords.size() - 1);
 	bool ifS(false);
 	if (S <= 72)
 	{
@@ -109,6 +137,10 @@ void cardDraw(void)
 		{
 			++countSS;
 			std::cout << "SS!!!!!!!!!!" << std::endl;
+			gachaRecords[i].push_back("SS");
+			std::vector<std::string> nextRecord;
+			gachaRecords.push_back(nextRecord);
+			++i;
 			A = 0;
 			AA = false;
 			S = 0;
@@ -125,6 +157,10 @@ void cardDraw(void)
 			{
 				++countSS;
 				std::cout << "SS!!!!!!!!!!" << std::endl;
+				gachaRecords[i].push_back("SS");
+				std::vector<std::string> nextRecord;
+				gachaRecords.push_back(nextRecord);
+				++i;
 				A = 0;
 				AA = false;
 				S = 0;
@@ -134,6 +170,7 @@ void cardDraw(void)
 			{
 				++countS;
 				std::cout << "S!!!!!" << std::endl;
+				gachaRecords[i].push_back("S");
 				A = 0;
 				AA = false;
 				S = 0;
@@ -169,6 +206,7 @@ void cardDraw(void)
 			{
 				++countAA;
 				std::cout << "AA!!!" << std::endl;
+				gachaRecords[i].push_back("AA");
 				A = 0;
 				AA = false;
 			}
@@ -183,6 +221,7 @@ void cardDraw(void)
 				{
 					++countAA;
 					std::cout << "AA!!!" << std::endl;
+					gachaRecords[i].push_back("AA");
 					A = 0;
 					AA = false;
 				}
@@ -190,6 +229,7 @@ void cardDraw(void)
 				{
 					++countA;
 					std::cout << "A!" << std::endl;
+					gachaRecords[i].push_back("A");
 					A = 0;
 					AA = true;
 				}
@@ -200,6 +240,7 @@ void cardDraw(void)
 			++A;
 			++countB;
 			std::cout << "B" << std::endl;
+			gachaRecords[i].push_back("B");
 		}
 	}
 	return;
